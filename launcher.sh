ad5e77b0088670f958108fd90c772754504544aa
0Blanck0 <alexandre.elisab8@gmail.com>
2020-11-11 23:12:06 +0100

    Last update

---
+++ launcher.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+clear
+
+echo -e "Choose your game mode"
+echo -ne "\n"
+echo "---------------------Options---------------------"
+echo "1. Basic  |  2. Dynamic Player | 3. Level and XP"
+echo -ne "\n"
+
+read player_action
+
+if [[ ($player_action == Basic) || ($player_action == basic) ]]; then
+    player_action=1
+elif [[ ($player_action == "dynamic player") || ($player_action == dynamic) || ($player_action == "Dynamic player") || ($player_action == Dynamic) || ($player_action == "Dynamic Player")]]; then
+    player_action=2
+fi
+
+clear
+
+if [[ $player_action == 1 ]]; then
+    cd base_game/
+    echo "Basic game load..."
+    sleep 2
+elif [[ $player_action == 2 ]]; then
+    cd dynamic_characters/
+    echo "Dynamic mode load..."
+    sleep 2
+else
+    cd level_and_experience/
+    echo "Level mode load..."
+    sleep 2
+fi
+
+source hyrule_castle.sh
